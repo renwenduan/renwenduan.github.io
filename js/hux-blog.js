81b9219eb850af6c141fcc3048792febd1e474fa
Renwen Duan <719756810@qq.com>
2017-07-20 20:30:31 +0800

    renew post

--- js/hux-blog.js
+++ js/hux-blog.js
@@ -82,3 +82,17 @@ jQuery(document).ready(function($) {
             });
     }
 });
+
+// add baiduspider tools
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
